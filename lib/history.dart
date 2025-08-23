16a868906856221f37f57ed7a962ec453f262271
Ardawan M.Amin <ardawan524@gmail.com>
2025-08-24 02:14:31 +0300

    Add files via upload

---
+++ lib/history.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: camel_case_types, use_key_in_widget_constructors, prefer_const_constructors
+
+
+import 'package:flutter/material.dart';
+
+class History extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      body: Center(
+        child: Text("Nothing To Show Here",style: TextStyle(fontSize: 30),),
+      ),
+    );
+  }
+}
